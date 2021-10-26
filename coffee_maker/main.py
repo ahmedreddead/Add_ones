d7b86e82305d33ef5e1b3fa642a3ee6497c52b48
ahmed mohamed <45078408+ahmedreddead@users.noreply.github.com>
2021-10-26 19:45:02 +0200

    Update main.py

--- coffee_maker/main.py
+++ coffee_maker/main.py
@@ -1,9 +1,28 @@
-import threading
 import time
 from zk import ZK, const
 import paho.mqtt.client as mqtt
 import socket
 broker_address = "62.210.9.28"
+def live () :
+    try : 
+        zk = ZK(finalip, port=4370, timeout=5, password=0, force_udp=True, ommit_ping=True)
+        conn = zk.connect()
+        conn.enable_device()
+        for attendance in conn.live_capture(10):
+            if attendance is None:
+                pass
+            else:
+                print("creating new instance")
+                client = mqtt.Client("P1")  # create new instance
+                print("connecting to broker")
+                client.connect(broker_address)  # connect to broker
+                client.subscribe("Attendance")
+                print(attendance)
+                client.publish("Attendance","ON")
+                client.publish("Attendance/ID", str(attendance))
+    except : 
+        live ()
+            
 finalip = ''
 for i in range(99,150) :
     #196.221.205.166
@@ -19,39 +38,7 @@ for i in range(99,150) :
         break
     socket_obj.close()
 print(finalip)
-def live () :
-    
-    zk = ZK(finalip, port=4370, timeout=5, password=0, force_udp=True, ommit_ping=True)
-    conn = zk.connect()
-    conn.enable_device()
 
-    for attendance in conn.live_capture(10):
-        if attendance is None:
-            pass
-        else:
-            print("creating new instance")
-            client = mqtt.Client("P1")  # create new instance
-            print("connecting to broker")
-            client.connect(broker_address)  # connect to broker
-            client.subscribe("Attendance")
-            print(attendance)
-            client.publish("Attendance","ON")
-            client.publish("Attendance/ID", str(attendance))
-            
-zk = ZK(finalip, port=4370, timeout=5, password=0, force_udp=True, ommit_ping=True)
-conn = zk.connect()
-conn.enable_device()
-conn.read_sizes()
-print(conn)
-for attendance in conn.live_capture(10):
-    if attendance is None:
-        pass
-    else:
-        print("creating new instance")
-        client = mqtt.Client("P1")  # create new instance
-        print("connecting to broker")
-        client.connect(broker_address)  # connect to broker
-        client.subscribe("Attendance")
-        print(attendance)
-        client.publish("Attendance","ON")
-        client.publish("Attendance/ID", str(attendance))
+live()
+
+    
