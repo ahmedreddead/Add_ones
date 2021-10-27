8e70e521236d38cbff702e6f57959cff82833563
ahmed mohamed <45078408+ahmedreddead@users.noreply.github.com>
2021-10-27 20:19:42 +0200

    Update main.py

--- LastAttendance/main.py
+++ LastAttendance/main.py
@@ -1,30 +1,10 @@
-import time
 import threading
-
+import time
+import json
 from zk import ZK, const
 import paho.mqtt.client as mqtt
 import socket
 broker_address = "62.210.9.28"
-def live () :
-    Thread  = threading.Timer(20.0, live)
-    Thread.start()
-    zk = ZK(finalip, port=4370, timeout=5, password=0, force_udp=True, ommit_ping=True)
-    conn = zk.connect()
-    conn.enable_device()
-    for attendance in conn.live_capture(10):
-        if attendance is None:
-            pass
-        else:
-            print("creating new instance")
-            client = mqtt.Client("P1")  # create new instance
-            print("connecting to broker")
-            client.connect(broker_address)  # connect to broker
-            client.subscribe("Attendance")
-            print(attendance)
-            client.publish("Attendance","ON")
-            client.publish("Attendance/ID", str(attendance))
-
-            
 finalip = ''
 for i in range(120,150) :
     #196.221.205.166
@@ -40,9 +20,55 @@ for i in range(120,150) :
         break
     socket_obj.close()
 print(finalip)
+def live () :
+    Thread  = threading.Timer(60.0, live)
+    Thread.start()
+    listOfId = []
+    listOfDate = []
+    listOfTime = []
+    zk = ZK(finalip, port=4370, timeout=5, password=0, force_udp=True, ommit_ping=True)
+    conn = zk.connect()
+    conn.enable_device()
+    attendances = conn.get_attendance()
+    print (type (attendances) )
+    for i in attendances : 
+        listvalue = str(i).split (" ")
+        listOfId.append(listvalue[1])
+        listOfDate.append(listvalue[3])
+        listOfTime.append(listvalue[4])
+    #print(listOfId[index])
+    #print(listOfDate)
+    #print(listOfTime)
+    Nameslist =[]
+    idlist = []
+    jsonlist = []
+    conn.delete_user(user_id=9)
+    users = conn.get_users()
+    for user in users:
+        Nameslist.append(user.name)
+        idlist.append(user.user_id)
+    
+    print(Nameslist)
+    print(idlist)
+    for n,name in zip (idlist,Nameslist) :
+        for i in reversed( range(len(listOfId)) ) :
+            if listOfId [i] == n :
+                index = i 
+                #print(listOfId[index])
+                print(name)
+                print(listOfDate[index])
+                print(listOfTime[index])
+                jsonname = { "Name": name , "Date": listOfDate[index] , "Time": listOfTime[index]}
+                print (json.dumps (jsonname))
+                jsonlist.append (json.dumps (jsonname))
+                break 
+
+    print("creating new instance")
+    client = mqtt.Client("P1")  # create new instance
+    print("connecting to broker")
+    client.connect(broker_address)  # connect to broker
+    client.subscribe("LastAttendance")
+    for i in range(len (jsonlist)) :
+        client.publish("LastAttendance/"+ str (i), str(jsonlist[i]))
 
 live()
-
-    
-
-    
