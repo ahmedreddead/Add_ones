4caa3232b90463ff5bbc881852263456dbda8cc7
ahmed mohamed <45078408+ahmedreddead@users.noreply.github.com>
2021-11-01 15:37:36 +0200

    Update main.py

--- LastAttendance/main.py
+++ LastAttendance/main.py
@@ -4,71 +4,90 @@ import json
 from zk import ZK, const
 import paho.mqtt.client as mqtt
 import socket
+
 broker_address = "62.210.9.28"
 finalip = ''
-for i in range(120,150) :
-    #196.221.205.166
-    #192.168.0.100
+for i in range(120, 150):
+    # 196.221.205.166
+    # 192.168.0.100
     socket_obj = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     socket.setdefaulttimeout(1)
-    ip = "196.221.205."+str(i)
-    result = socket_obj.connect_ex((ip,4370))
+    ip = "196.221.205." + str(i)
+    result = socket_obj.connect_ex((ip, 4370))
     print(result)
-    if result == 0 :
+    if result == 0:
         finalip = ip
         socket_obj.close()
         break
     socket_obj.close()
 print(finalip)
-def live () :
-    Thread  = threading.Timer(60.0, live)
+
+
+def live():
+    Thread = threading.Timer(60.0, live)
     Thread.start()
     listOfId = []
     listOfDate = []
     listOfTime = []
-    zk = ZK(finalip, port=4370, timeout=5, password=0, force_udp=True, ommit_ping=True)
+    zk = ZK(finalip, port=4370, timeout=15, password=0, force_udp=True, ommit_ping=True)
     conn = zk.connect()
     conn.enable_device()
     attendances = conn.get_attendance()
-    print (type (attendances) )
-    for i in attendances : 
-        listvalue = str(i).split (" ")
+    print(type(attendances))
+    for i in attendances:
+        listvalue = str(i).split(" ")
         listOfId.append(listvalue[1])
         listOfDate.append(listvalue[3])
         listOfTime.append(listvalue[4])
-    #print(listOfId[index])
-    #print(listOfDate)
-    #print(listOfTime)
-    Nameslist =[]
+    # print(listOfId[index])
+    # print(listOfDate)
+    # print(listOfTime)
+    Nameslist = []
     idlist = []
     jsonlist = []
-    conn.delete_user(user_id=9)
     users = conn.get_users()
     for user in users:
         Nameslist.append(user.name)
         idlist.append(user.user_id)
-    
+
     print(Nameslist)
     print(idlist)
-    for n,name in zip (idlist,Nameslist) :
-        for i in reversed( range(len(listOfId)) ) :
-            if listOfId [i] == n :
-                index = i 
-                #print(listOfId[index])
+    for n, name in zip(idlist, Nameslist):
+        for i in reversed(range(len(listOfId))):
+            if listOfId[i] == n:
+                index = i
+                # print(listOfId[index])
                 print(name)
                 print(listOfDate[index])
                 print(listOfTime[index])
-                jsonname = { "Name": name , "Date": listOfDate[index] , "Time": listOfTime[index]}
-                print (json.dumps (jsonname))
-                jsonlist.append (json.dumps (jsonname))
-                break 
+
+                lastDate = listOfDate[index]
+                lastTime = listOfTime[index]
+                listTimesOfCurrentDay = []
+                for i in reversed(range(len(listOfDate))):
+                    if lastDate == listOfDate[i] :
+                        listTimesOfCurrentDay.append(listOfTime[i])
+                firstTime = listTimesOfCurrentDay[-1]
+                Lasthours, Lastmin, Lastsec = lastTime.split(":")
+                firsthours, firstmin, firstsec = firstTime.split(":")
+
+                hours =   str ( int(Lasthours) - int (firsthours)  )
+
+                jsonname = {"Name": name, "Date": listOfDate[index], "FirstAttendance": firstTime  , "LastAttendance": lastTime , "WorkingHours" :hours}
+                print(json.dumps(jsonname))
+                jsonlist.append(json.dumps(jsonname))
+
+                break
+
+
 
     print("creating new instance")
     client = mqtt.Client("P1")  # create new instance
     print("connecting to broker")
     client.connect(broker_address)  # connect to broker
     client.subscribe("LastAttendance")
-    for i in range(len (jsonlist)) :
-        client.publish("LastAttendance/"+ str (i), str(jsonlist[i]))
+    for i in range(len(jsonlist)):
+        client.publish("LastAttendance/" + str(i), str(jsonlist[i]))
+
 
 live()
