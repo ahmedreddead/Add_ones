d23868fdc15fe027e1d0cb67b812592e8cb23a5a
ahmed mohamed <45078408+ahmedreddead@users.noreply.github.com>
2021-11-15 14:39:46 +0200

    Update main.py

--- LastAttendance/main.py
+++ LastAttendance/main.py
@@ -6,22 +6,7 @@ import paho.mqtt.client as mqtt
 import socket
 
 broker_address = "62.210.9.28"
-finalip = ''
-for i in range(120, 150):
-    # 196.221.205.166
-    # 192.168.0.100
-    socket_obj = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    socket.setdefaulttimeout(1)
-    ip = "196.221.205." + str(i)
-    result = socket_obj.connect_ex((ip, 4370))
-    print(result)
-    if result == 0:
-        finalip = ip
-        socket_obj.close()
-        break
-    socket_obj.close()
-print(finalip)
-
+finalip = '192.168.0.107'
 
 def live():
     Thread = threading.Timer(3600.0, live)
