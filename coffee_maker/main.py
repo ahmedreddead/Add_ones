c827a5fdd19cb6a7ed7d0251606f4994876e84e2
ahmed mohamed <45078408+ahmedreddead@users.noreply.github.com>
2021-11-28 15:38:46 +0200

    Update main.py

--- coffee_maker/main.py
+++ coffee_maker/main.py
@@ -4,7 +4,7 @@ import threading
 from zk import ZK, const
 import paho.mqtt.client as mqtt
 import socket
-broker_address = "62.210.9.28"
+broker_address = "192.168.0.100"
 finalip = '192.168.0.107'
 def live () :
     Thread  = threading.Timer(25.0, live)
