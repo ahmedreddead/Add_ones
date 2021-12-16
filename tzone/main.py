aa19e39c84ff660fe43346fa4d2209e87d6dbab8
ahmed mohamed <45078408+ahmedreddead@users.noreply.github.com>
2021-12-16 13:53:42 +0200

    Update main.py

--- tzone/main.py
+++ tzone/main.py
@@ -106,8 +106,8 @@ def mqtt_send ():
     client.publish("Tzone/"+ID+'/Time', Time)
 
 
-HOST = '0.0.0.0'
-PORT = 8123
+HOST = ''
+PORT = 8000
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen(5)
