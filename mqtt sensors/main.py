1122630a8f2a8ffb1101fb33dba524e2e9452015
ahmed mohamed <45078408+ahmedreddead@users.noreply.github.com>
2022-02-10 17:02:00 +0200

    Update main.py

--- mqtt sensors/main.py
+++ mqtt sensors/main.py
@@ -5,24 +5,36 @@ import paho.mqtt.client as mqtt
 import socket
 broker_address = "192.168.0.100"
 def live () :
-    Thread  = threading.Timer(60.0, live)
+    Thread  = threading.Timer(120.0, live)
     Thread.start()
     print("creating new instance")
     client = mqtt.Client("P1")  # create new instance
     print("connecting to broker")
     #client.username_pw_set(username="mqtt-user", password="0000")
+    client.username_pw_set(username="homeassistant", password="yayeeheed8eezaechiwu4thahbaij2eiki0eim8Bo1chahbatief4Ohs1mait0Ph")
     client.connect(broker_address)  # connect to broker
-    client.publish("hu","55 %")
+    client.publish("hu","100 %")
     client.publish("hu1","58 %")
     client.publish("hu2","40 %")
     client.publish("hu3","46 %")
     client.publish("hu4","42 %")
+    client.publish("hu5","55 %")
+    client.publish("hu6","58 %")
+    client.publish("hu7","40 %")
+    client.publish("hu8","46 %")
+    client.publish("hu9","90 %")
 
-    client.publish("temp","22.5°C   55 %")
-    client.publish("temp1","21.4°C   58 %")
-    client.publish("temp2","19.8°C   40 %")
-    client.publish("temp3","15.2°C   46 %")
-    client.publish("temp4","10.3°C   42 %")
+    client.publish("temp","18.5°C")
+    client.publish("temp1","15.4°C")
+    client.publish("temp2","19.8°C")
+    client.publish("temp3","15.2°C")
+    client.publish("temp4","5.2°C")
+
+    client.publish("temp5","18.5°C")
+    client.publish("temp6","19.4°C")
+    client.publish("temp7","19.8°C")
+    client.publish("temp8","15.2°C")
+    client.publish("temp9","7.3°C")
 
 live()
 
